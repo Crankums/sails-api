220eaeee1c75ebdac47b00e56517d0823ede4d19
Craig_Donavin <crdonavin@gmail.com>
2020-11-15 16:12:36 -0600

    add response to index.js

--- api/controllers/home/index.js
+++ api/controllers/home/index.js
@@ -1,28 +1,13 @@
 module.exports = {
-
-
   friendlyName: 'Index',
-
-
   description: 'Index home.',
-
-
   inputs: {
-
   },
-
-
   exits: {
-
   },
 
-
-  fn: async function (inputs) {
-
+  fn: async function (_, exits) {
     // All done.
-    return;
-
+    exits.success({message: 'Sails API'});
   }
-
-
 };
