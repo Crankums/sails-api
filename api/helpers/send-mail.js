e2b1e00074eed2dd942a7d65d877915c9584c7ed
Craig_Donavin <crdonavin@gmail.com>
2020-11-19 22:27:00 -0600

    send-mail helper complete

--- api/helpers/send-mail.js
+++ api/helpers/send-mail.js
@@ -1,14 +1,14 @@
+var nodemailerSendgrid = require('nodemailer-sendgrid');
+const hbs = require('nodemailer-express-handlebars');
+
 module.exports = {
-
-
   friendlyName: 'Send mail',
-
-
   description: '',
-
-
   inputs: {
-
+    options: {
+      type: 'ref',
+      required: true,
+    },
   },
 
 
@@ -22,9 +22,33 @@ module.exports = {
 
 
   fn: async function (inputs) {
-    // TODO
+    const transporter = nodemailer.createTransport(
+      nodemailerSendgrid({
+        apiKey: sails.config.sendGridAPIKey || process.env.SENDGRID_API_KEY
+      })
+    );
+    transporter.use(
+      'compile',
+      hbs({
+        viewEngine: {
+          extName: '.hbs',
+          partialsDir: './views',
+          layoutsDir: './views',
+          defaultLayout: '',
+        },
+        viewPath: './views/',
+        extName: '.hbs',
+      })
+    );
+    try {
+      let emailOptions = {
+        from: 'Sails-Api <alert@sailsapi.com',
+        ...inputs.options,
+      };
+      await transporter.sendmail(emailOptions);
+    } catch (error) {
+      sails.log(error);
+    }
   }
-
-
 };
 
