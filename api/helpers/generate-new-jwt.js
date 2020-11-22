744682ab7de1f24b376b72e67aa185c8ab83b082
Craig_Donavin <crdonavin@gmail.com>
2020-11-22 00:19:08 -0600

    add jwt helper

---
+++ api/helpers/generate-new-jwt.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+
+  friendlyName: 'Generate new jwt',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+  },
+
+
+  fn: async function (inputs) {
+    // TODO
+  }
+
+
+};
+
