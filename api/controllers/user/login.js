38a6ac5dd8e903a79dbb9d836e9561a89cc93f2a
Craig_Donavin <crdonavin@gmail.com>
2020-11-21 23:31:16 -0600

    add user-login actions

--- api/controllers/user/login.js
+++ api/controllers/user/login.js
@@ -1,28 +1,69 @@
 module.exports = {
 
-
   friendlyName: 'Login',
-
 
   description: 'Login user.',
 
-
   inputs: {
-
+    email: {
+      type: 'string',
+      required: true,
+    },
+    password: {
+      type: 'string',
+      required: true,
+    },
   },
-
-
   exits: {
-
+    success: {
+      description: 'Login successful',
+    },
+    notAUser: {
+      statusCode: 404,
+      description: 'User not found',
+    },
+    passwordMismatch: {
+      statusCode: 401,
+      description: 'Password does not match',
+    },
+    operationalError: {
+      statusCode: 400,
+      description: 'Bad request or request error'
+    }
   },
-
 
   fn: async function (inputs) {
-
-    // All done.
-    return;
-
+    try {
+      const user = await User.findOne({ email: inputs.email});
+      if (!user) {
+        return exits.notAUser({
+          error: `An account belonging to ${inputs.email} was not found.`
+        });
+      }
+      await sails.helpers.passwords
+        .checkPassword(inputs.password, user.password)
+        .intercept('incorrect', (error) => {
+          exits.passwordMismatch({ error: error.message });
+        });
+      const token = await sails.helpers.generateNewJwtToken(user.email);
+      this.req.me = user;
+      return exits.success({
+        message: `${user.email} has been logged in.`,
+        data: user,
+        token,
+      });
+    } catch (error) {
+      sails.log.error(error);
+      if (error.isOperational) {
+        return exits.operationalError({
+          message: `Error logging in ${inputs.email}`,
+          error: error.raw,
+        });
+      }
+      return exits.error({
+        message: `Error logging in user ${inputs.email}`,
+        error: error.message,
+      });
+    }
   }
-
-
 };
