bd56b6178bbd035770612c2284f24902649cf4db
Craig_Donavin <crdonavin@gmail.com>
2020-11-23 22:19:50 -0600

    add inputs.email, exits.success, and complete fn

--- api/controllers/user/forgot-password.js
+++ api/controllers/user/forgot-password.js
@@ -8,17 +8,35 @@ module.exports = {
 
 
   inputs: {
+    email: {
+      description: 'The email address of the user who wants to recover their password.',
+      example: 'albus@dumbledore.com',
+      type: 'string',
+      required: true
+    },
 
   },
 
 
   exits: {
+    success: {
+      description: 'Email matched a user and a recovery email might have been sent.'
+    },
 
   },
 
 
-  fn: async function (inputs) {
-
+  fn: async function (inputs, exits) {
+    const user = User.findOne({email: inputs.email});
+    if (!user) {
+      return;
+    }
+    const token = sails.helpers.strings.random('url-friendly');
+    await User.update({id: user.id}).set({
+      passwordResetToken: token,
+      passwordResetTokenExpiresAt: Date.now() + sails.config.custom.passwordResetTokenTTL,
+    });
+    const recoveryLink = `${sails.config.custom.baseUrl}/user/reset-password?token=${token}`;
     // All done.
     return;
 
